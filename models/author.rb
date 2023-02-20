fce3e7b96c7806a82e2fec011355436fb776d7b5
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-02-20 14:57:48 +0300

    magazine and add_article methods

--- models/author.rb
+++ models/author.rb
@@ -10,4 +10,15 @@ class Author
         Article.all.filter {|article|
         article.author == @name }
     end
+    #Returns a unique array of Magazine instances for which the author has contributed to
+    def magazines
+        #from articles get magazine instances
+        articles.map { |article| article.magazine}.uniq
+    end
+
+    #creates a new Article instance and associates it with that author and that magazine.
+    def add_article(magazine,title)
+     Article.new(self,magazine,title)
+    end
+
 end
