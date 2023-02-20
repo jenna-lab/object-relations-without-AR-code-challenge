67a92e1a26c02777c6e5154fe26b000f8cb9d6ff
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-02-20 14:56:34 +0300

    added articles method

--- models/author.rb
+++ models/author.rb
@@ -1,2 +1,13 @@
 class Author 
+    attr_reader :name
+    def initialize(name)
+        @name = name
+    end
+
+    #Returns an array of Article instances the author has written
+    def articles
+        #filter from articles
+        Article.all.filter {|article|
+        article.author == @name }
+    end
 end
